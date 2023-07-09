5099d655e82233cd820a58698d8af1a18f23c68e
FreddieLi <b09613@gmail.com>
2023-07-09 22:11:48 +0800

    For 01_Base, Clear output of All cells

--- 01_base/练习/数羊问题.py
+++ 01_base/练习/数羊问题.py
@@ -8,24 +8,59 @@
 '''
 
 
-i = 0
-while True:
-    i += 1
-    left_endpoint = 1 + 5 * ( i - 1 )
-    right_endpoint = 1 + 5 * i
-    for i in range(left_endpoint, right_endpoint):
-        print(str(i)+'只羊')
-    answer = input('睡着了吗？回答是或否：')
-    if answer == '是':
-        break
-    print('继续数羊')
-print('终于睡着了')
 
-#方法二
-睡觉的状态 = '还没睡'
-a = 0
-while 睡觉的状态 != '睡着': # 只要不是睡着，就继续数
-    a +=1
-    print(str(a)+'只羊')
-    if a%9 == 0 : # %是取余数 每次数5只羊
-        睡觉的状态 = input('睡着了嘛？')
+
+
+
+
+
+i = 0
+while (1):
+    i+=1
+    print("sheep%d"%(i))
+    if (i%5 == 0):
+        ans=input("睡着了吗？")
+        # case insensitive compare
+        if (ans.casefold() =='y'):
+            print("sleep already!")
+            break
+        else:
+            print("keep counting...")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# i = 0
+# while True:
+#     i += 1
+#     left_endpoint = 1 + 5 * ( i - 1 )
+#     right_endpoint = 1 + 5 * i
+#     for i in range(left_endpoint, right_endpoint):
+#         print(str(i)+'只羊')
+#     answer = input('睡着了吗？回答是或否：')
+#     if answer == '是':
+#         break
+#     print('继续数羊')
+# print('终于睡着了')
+
+# #方法二
+# 睡觉的状态 = '还没睡'
+# a = 0
+# while 睡觉的状态 != '睡着': # 只要不是睡着，就继续数
+#     a +=1
+#     print(str(a)+'只羊')
+#     if a%9 == 0 : # %是取余数 每次数5只羊
+#         睡觉的状态 = input('睡着了嘛？')
