84203a59195841b6854f46793d486f6b33ca8a50
FreddieLi <b09613@gmail.com>
2023-09-29 23:01:15 +0800

    Update RobotGraph

--- 01_base/练习/RobotGraph.py
+++ 01_base/练习/RobotGraph.py
@@ -1,12 +1,12 @@
 """
-Given a 6x6 grid matrix, Robot is given the knowledge of Grid Occupancy Mapping, current location, and the destination as illustrated below:
+Given a 7x7 grid matrix,index starting from 0 to 6, Robot is given the knowledge of Grid Occupancy Mapping, current location, and the destination as illustrated below:
 Current robot's location is at (1, 3), destination is at (6, 2), and the occupied cells are (3,2),(3,3),(3,4),(4,2),(4,3),(4,4).
 Write a solution class to search for a navigable path from current location to the destination, 
 and validate your solution against the illustrated sample in the main() function.
 
 Note that the robot can only move to adjacent cells either vertically or horizontally but not diagonally!
 """
-# This answer is from chatGPT.  
+# This answer is from chatGPT.  BFS
 from collections import deque
 
 class RobotGrid:
@@ -17,12 +17,12 @@ class RobotGrid:
         self.destination = destination
 
     def is_valid(self, x, y):
-        return 1 <= x <= self.grid_size[0] and 1 <= y <= self.grid_size[1] and (x, y) not in self.occupied_cells
+        return 0 <= x <= self.grid_size[0] and 0 <= y <= self.grid_size[1] and (x, y) not in self.occupied_cells
 
     def find_path(self):
         visited = set()
-        queue = deque([(self.current_location, [])])
-
+        queue = deque([(self.current_location, [])]) # tuple and a empty list
+        cnt = 1
         while queue:
             current, path = queue.popleft()
             x, y = current
@@ -40,12 +40,15 @@ class RobotGrid:
 
             for move in moves:
                 if self.is_valid(*move):
-                    queue.append((move, path + [current]))
+                    queue.append((move, path + [current])) #append tuple(move) and list(path + [current])
+            # print(cnt,end="")
+            # print(queue)
+            cnt+=1
 
         return None  # No path found
 
 def main():
-    grid_size = (6, 6)
+    grid_size = (7, 7)
     occupied_cells = [(3, 2), (3, 3), (3, 4), (4, 2), (4, 3), (4, 4)]
     current_location = (1, 3)
     destination = (6, 2)
